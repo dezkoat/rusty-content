f26158583c1922833ca204f11dfafa9841a91eaf
dezkoat <dezkoat13@gmail.com>
2019-11-18 00:19:23 +0700

    Use 4 spaces

--- src/services/post/mod.rs
+++ src/services/post/mod.rs
@@ -10,10 +10,10 @@ use service::post_delete;
 
 pub fn define_router(router: &mut Router) {
 
-  router.get   ("/post",          post_read_all, "post_read_all");
-  router.get   ("/post/:post_id", post_read,     "post_read");
-  router.post  ("/post",          post_create,   "post_create");
-  router.put   ("/post/:post_id", post_update,   "post_update");
-  router.delete("/post/:post_id", post_delete,   "post_delete");
+    router.get   ("/post",          post_read_all, "post_read_all");
+    router.get   ("/post/:post_id", post_read,     "post_read");
+    router.post  ("/post",          post_create,   "post_create");
+    router.put   ("/post/:post_id", post_update,   "post_update");
+    router.delete("/post/:post_id", post_delete,   "post_delete");
 
 }
--- src/services/post/service.rs
+++ src/services/post/service.rs
@@ -2,7 +2,7 @@ use iron::prelude::*;
 use iron::status;
 
 pub fn post_read_all(_req: &mut Request) -> IronResult<Response> {
-  Ok(Response::with((status::Ok, "This should return post".to_owned())))
+    Ok(Response::with((status::Ok, "This should return post".to_owned())))
 }
 
 pub fn post_read(_req: &mut Request) -> IronResult<Response> {
@@ -10,13 +10,13 @@ pub fn post_read(_req: &mut Request) -> IronResult<Response> {
 }
 
 pub fn post_create(_req: &mut Request) -> IronResult<Response> {
-  Ok(Response::with((status::Ok, "This should return post".to_owned())))
+    Ok(Response::with((status::Ok, "This should return post".to_owned())))
 }
 
 pub fn post_update(_req: &mut Request) -> IronResult<Response> {
-  Ok(Response::with((status::Ok, "This should return post".to_owned())))
+    Ok(Response::with((status::Ok, "This should return post".to_owned())))
 }
 
 pub fn post_delete(_req: &mut Request) -> IronResult<Response> {
-  Ok(Response::with((status::Ok, "This should return post".to_owned())))
+    Ok(Response::with((status::Ok, "This should return post".to_owned())))
 }
