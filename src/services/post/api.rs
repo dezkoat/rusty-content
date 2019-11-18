155425e47e9a46f901b561af63baaac91df55e0e
dezkoat <dezkoat13@gmail.com>
2019-11-18 23:24:24 +0700

    temp

--- src/services/post/api.rs
+++ src/services/post/api.rs
@@ -1,10 +1,11 @@
 pub mod PostApi {
 
+    use crate::services::post::service::PostService;
     use iron::prelude::*;
     use iron::status;
 
     pub fn post_read_all(_req: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "asdsad")))
+        Ok(Response::with((status::Ok, PostService::post_read_all())))
     }
 
 }
